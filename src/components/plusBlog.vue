8153858b7c2d05a9c07f884ae92fbce61c361da5
KingAJ19 <anelesokwane@gmail>
2020-10-26 16:50:04 +0200

    Select Box Binding

--- src/components/plusBlog.vue
+++ src/components/plusBlog.vue
@@ -16,6 +16,10 @@
             <label>Momoshiki</label>
             <input type="checkbox" value="Kenshiki" v-model="blog.categories"/>
         </div>
+        <label>Author:</label>
+        <select v-model="blog.author">
+            <option v-for="author in authors">{{ author }}</option>
+        </select>
     </form>
     <div id="preview">
         <h3>Preview Blog</h3>
@@ -26,6 +30,7 @@
         <ul>
             <li v-for="category in blog.categories">{{ category }}</li>
         </ul>
+        <p>Author: {{ blog.author }}</p>
     </div>
     
   </div>
@@ -40,8 +45,10 @@ export default {
       blog: {
           title: "",
           content: "",
-          categories: []
-      }
+          categories: [],
+          author: ""
+      },
+      authors: ['Minato Namikaze', 'Kakashi Hatake', 'Tobirama Senju']
     }
   },
   methods:{
