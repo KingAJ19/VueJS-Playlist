13bac8bce915d9eb601090d65c1360cd67067321
KingAJ19 <anelesokwane@gmail>
2020-10-27 12:26:30 +0200

    Routes

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import showBlogs from './components/showBlogs.vue';
+import plusBlog from './components/plusBlog.vue';
+
+
+export default [
+    {path: '/', component: showBlogs },
+    {path: '/add', component: plusBlog}
+]
