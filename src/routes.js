6d21bdac602378bf7b13d858f76749146e03bc03
KingAJ19 <anelesokwane@gmail>
2020-10-27 13:57:50 +0200

    Route Parameters

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import showBlogs from './components/showBlogs.vue';
 import plusBlog from './components/plusBlog.vue';
+import singleBlog from './components/singleBlog.vue';
 
 
 export default [
     {path: '/', component: showBlogs },
-    {path: '/add', component: plusBlog}
+    {path: '/add', component: plusBlog},
+    {path: '/blog/:id', component: singleBlog}
 ]
